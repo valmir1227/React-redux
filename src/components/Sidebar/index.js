69e3efa2dcbe7b49ef0d09f23f6e32c4bf8096c0
Valmir Almeida <almeidavalmir76@gmail.com>
2022-03-20 18:38:22 -0300

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+import { connect } from "react-redux";
+
+import * as CourseActions from "../../store/actions/course";
+
+const Sidebar = ({ modules, dispatch }) => (
+  <aside>
+    {modules.map((module) => (
+      <div key={module.id}>
+        <strong>{module.title}</strong>
+        <ul>
+          {module.lessons.map((lesson) => (
+            <li key={lesson.id}>
+              {lesson.title}
+              <button
+                onClick={() =>
+                  dispatch(CourseActions.toggleLesson(module, lesson))
+                }
+              >
+                Select
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </aside>
+);
+
+export default connect((state) => ({ modules: state.course.modules }))(Sidebar);
